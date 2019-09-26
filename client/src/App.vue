113e33f722038e01f22d7f3dcce0da16617c4d67
wwyy <1114789207@qq.com>
2019-09-26 10:27:48 +0800

    add mainview

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
-
+<MainView></MainView>
   </div>
 </template>
 
 <script>
-
+import MainView from'./components/MainView'
 export default {
   name: 'app',
   components:{
+         MainView
   }
 }
 </script>
 
 <style>
-#app {
+
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
+} */
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,10 @@ import VueRouter from 'vue-router';
 import App from './App.vue';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import "ant-design-vue/dist/antd.css"
+import Antd from 'ant-design-vue'
 Vue.use(VueAxios, axios);
+Vue.use(Antd);
 Vue.config.productionTip = false;
 
 new Vue({
